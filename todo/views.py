cf0407121796e935d1e87685652d0f5c1520d5a1
Ben Smith <benjaminsmith1981@googlemail.com>
2022-06-21 20:00:44 +0000

    Added adding to database

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Item
 # Create your views here.
 def get_todo_list(request):
     #return HttpResponse(request, 'todo/todo_list.html')
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_item.html')
+
